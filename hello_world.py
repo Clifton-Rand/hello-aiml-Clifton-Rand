f0d75e604895bdc8c730a7c8447a7ae6628e80f8
Clifton-Rand <Clifton.C.Rand@gmail.com>
2025-12-18 21:24:59 -0500

    added my introduction

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,8 @@
 # Hello AI/ML Bootcamp!
 # Edit the variables below to introduce yourself
 
-name = "Your Name Here"
-fun_fact = "One fun fact about me"
+name = "Clifton Rand"
+fun_fact = "I like the washington capitals hockey team"
 
 # Don't change anything below this line
 print(f"Hello! I'm {name}.")
